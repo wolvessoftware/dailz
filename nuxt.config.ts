67ac94143bbe99732179a953ab0b5415a6e5bcbc
Marcel Pfeifer <marcel@wolves.ink>
2022-11-06 20:23:54 +0100

    feat: :sparkles: Basic setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  app: {
+    head: {
+      title: "Dailz - Minimal day planner",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Minimal, but beautiful day planner. Focus on what you need to get done.",
+        },
+      ],
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxtjs/supabase"],
   colorMode: {
     classSuffix: "",
   },
